1364189bdc72a52a6bbdbfbd246671cf30a6de7d
Andre Russle <codewithruss@gmail.com>
2022-09-10 23:24:47 +0600

    modified

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,31 +9,32 @@ const Counter = () => {
   const minus = () => {
     setCount(count - 1);
   };
-  const zero = () => {
+  const reset = () => {
     setCount(0);
   };
-  let P = false;
-  let M = false;
-  if (count === 5) {
-    P = true;
-  }
-  if (count === -5) {
-    M = true;
-  }
+
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
         <h2 className="card__title">Count : {count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn" disabled={P} onClick={plus}>
+          <button
+            className="btn card__btn"
+            disabled={count === 5 ? true : false}
+            onClick={plus}
+          >
             +
           </button>
-          <button className="btn card__btn" disabled={M} onClick={minus}>
+          <button
+            className="btn card__btn"
+            disabled={count === -5 ? true : false}
+            onClick={minus}
+          >
             {" "}
             -
           </button>
-          <button className="btn card__btn" onClick={zero}>
+          <button className="btn card__btn" onClick={reset}>
             0
           </button>
         </div>
