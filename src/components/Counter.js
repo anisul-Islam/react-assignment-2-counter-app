26dd2112e49736e58ef15179582196498181395e
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-09-10 11:56:02 +0600

    added increment button event

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const increaseBtn = () => {
+    // eslint-disable-next-line prettier/prettier
+    setCounter(counter + 1)
+  };
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count :{counter}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button className="btn card__btn" onClick={increaseBtn} disabled={counter === 5}>
+            +
+          </button>
+          <button className="btn card__btn"> - </button>
+          <button className="btn card__btn"> 0 </button>
         </div>
       </div>
     </div>
