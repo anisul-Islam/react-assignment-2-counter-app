7235a4092fc9128bc4aee34712ec1821caae14d3
suny-shaikh <mdsunyshaikh@gmail.com>
2022-09-11 23:47:17 +0600

    update Counter.js and index.css

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : {count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button
+            className="btn card__btn"
+            onClick={() => setCount(count + 1)}
+            disabled={count >= 5}>
+            +
+          </button>
+          <button
+            className="btn card__btn"
+            onClick={() => setCount(count - 1)}
+            disabled={count <= -5}>
+            -
+          </button>
+          <button className="btn card__btn" onClick={() => setCount(0)}>
+            0
+          </button>
         </div>
       </div>
     </div>
