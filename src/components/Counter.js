3ecc50c20df291622fb3ea3391b275c0c2d287d9
0xMahabub <mahabub212002072@gmail.com>
2022-09-12 18:16:06 +0600

    assignment2--done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [btn, SetBtn] = useState({
+    increment: false,
+    decrement: false
+  });
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  useEffect(() => {
+    if (count == 5) {
+      SetBtn({ ...btn, increment: true });
+    } else if (count == -5) {
+      SetBtn({ ...btn, decrement: true });
+    } else {
+      SetBtn({
+        ...btn,
+        increment: false,
+        decrement: false
+      });
+    }
+  }, [count]);
+
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : {count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button disabled={btn.increment} onClick={increment} className="btn card__btn">
+            +
+          </button>
+          <button disabled={btn.decrement} onClick={decrement} className="btn card__btn">
+            -
+          </button>
+          <button onClick={reset} className="btn card__btn">
+            0
+          </button>
         </div>
       </div>
     </div>
