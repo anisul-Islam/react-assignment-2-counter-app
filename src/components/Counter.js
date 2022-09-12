04412a8eb17fbaed5758ebbf0109bb8634516308
RunTimeTerrorBd <113308395+RunTimeTerrorBd@users.noreply.github.com>
2022-09-12 12:35:51 +0600

    Update Counter.js
    My Home Wrok Done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,40 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  function incrementCount() {
+    setCount(count + 1);
+  }
+
+  function decrementCount() {
+    setCount(count - 1);
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  function resetCount() {
+    setCount(0);
+  }
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
-        <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+        <h2 className="card__title">Count : {count}</h2>
+        <div className="card__btn">
+          <button
+            className="btn card__btn : btn:disabled "
+            onClick={incrementCount}
+            disabled={count === 5}>
+            +
+          </button>
+          <button
+            className="btn card__btn : btn:disabled"
+            onClick={decrementCount}
+            disabled={count === -5}>
+            -
+          </button>
+          <button className="btn card__btn" onClick={resetCount}>
+            0
+          </button>
         </div>
       </div>
     </div>
