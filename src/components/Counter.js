fcefbd3c8630451b08290fd8d456e48ce3da730b
kishor_biswas <biswaskishor03@gmail.com>
2022-10-19 09:38:21 +0530

    Second react assignment, counter app project

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,34 @@
-import React from 'react';
+/* eslint-disable */
+
+import React, { useState } from 'react';
 
 const Counter = () => {
+
+  const [increment, setImcement] = useState(0);
+
+
+  const Increment = () =>{
+    setImcement(increment + 1);
+  }
+
+  const Decrement = () => {
+    setImcement(increment - 1);
+  }
+
+
+  const Reset = () => {
+    setImcement(increment - increment);
+  }
+
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : {increment}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button className="btn card__btn" onClick={Increment} disabled={increment >= 5 ? true : false}>+</button>
+          <button className="btn card__btn" onClick={Decrement} disabled={increment <= -5 ? true : false}>-</button>
+          <button className="btn card__btn" onClick={Reset}>0</button>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
