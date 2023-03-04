bb36ae8f360d92f472eaee0f8e029c71fb7f4e92
MOtawash Semeen Chowdhury <motawash15-14166@diu.edu.bd>
2023-03-04 23:52:00 +0600

    assignmnt done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,19 @@
-import React from 'react';
+/* eslint-disable*/
+import React, { useState } from 'react';
 
 const Counter = () => {
+
+  const [value, setValue] = useState(0);
+
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : {value}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button className="btn card__btn" onClick={()=>setValue(value+1)} disabled={value>4?true:false}>+</button>
+          <button className="btn card__btn"  onClick={()=>setValue(value-1)} disabled={value<-4?true:false}>-</button>
+          <button className="btn card__btn"  onClick={()=>setValue(0)}>0</button>
         </div>
       </div>
     </div>
