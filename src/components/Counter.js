98239490b064a34311f8cc8c94a4808d078d861f
md-suhag <abdussalamsuhag104@gmail.com>
2023-09-15 08:58:16 +0600

    Assignment-2 Completed

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+
+  const [count, setCount] = useState(0)
+
+  const handleIncrement = ()=>{
+    setCount(count + 1);
+  }
+  const handleDecrement = ()=>{
+    setCount(count - 1);
+  }
+  const handleReset = ()=>{
+    setCount(count - count);
+  }
+
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : {count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button className="btn card__btn" onClick={handleIncrement} disabled={count === 5 ? true : false}>+</button>
+          <button className="btn card__btn" onClick={handleDecrement} disabled={count === -5 ? true : false}>-</button>
+          <button className="btn card__btn" onClick={handleReset}>0</button>
         </div>
       </div>
     </div>
