0021577794a85c5c179708fafcf9853d873879bc
nada <nadayahya2020@hotmail.com>
2023-10-09 13:40:35 -0700

    Done count app using useState()

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    count <= 4 ? setCount(count + 1) : '';
+  };
+  const handleResent = () => {
+    setCount(0);
+  };
+  const handleDecrement = () => {
+    count >= -4 ? setCount(count - 1) : '';
+  };
+
   return (
     <div className="counter center">
-      <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : {count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button onClick={handleIncrement} className="btn card__btn">
+            +
+          </button>
+          <button onClick={handleResent} className="btn card__btn">
+            0
+          </button>
+          <button onClick={handleDecrement} className="btn card__btn">
+            -
+          </button>
         </div>
       </div>
     </div>
