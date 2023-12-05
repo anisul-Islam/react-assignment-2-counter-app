bcb0d967f0499c1bab652efe0c1f6db9d93cca72
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2023-12-05 10:34:36 +0600

    completed assignment-2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,35 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
+import { useState } from 'react';
 
 const Counter = () => {
+  let [state, setstate] = useState(0);
+
+  const increment = () => {
+    return setstate((state) => state = state + 1)
+  }
+  const decrement = () => {
+    return setstate((state) => state = state - 1)
+  }
+
+  const reset = () => {
+    return setstate(state = 0)
+  }
+
   return (
-    <div className="counter center">
-      <h1 className="counter__title">Counter App</h1>
-      <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
-        <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+    <>
+      <div className="counter center">
+        <h1 className="counter__title">Counter App</h1>
+        <div className="card center">
+          <h2 className="card__title">Count : {state}</h2>
+          <div className="card__btns">
+            <button className="btn card__btn" onClick={increment} disabled={(state == 5) ? true : false}>+</button>
+            <button className="btn card__btn" onClick={decrement} disabled={(state == -5) ? true : false}>-</button>
+            <button className="btn card__btn" onClick={reset}>0</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
